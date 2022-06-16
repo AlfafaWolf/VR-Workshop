0e05a1f7abd8b7baf90cc89bf305bb613b2d7b88
AlfafaWolf <gabriel.meynard@gmail.com>
2022-06-16 15:16:55 -0300

    feat: add script to hide reticle on hover anchor

---
+++ Assets/Scripts/HideReticleOnHoverTeleportationAnchor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace VRWorkshop
+{
+    public class HideReticleOnHoverTeleportationAnchor : MonoBehaviour
+    {
+        [SerializeField] private GameObject reticleRender;
+    
+        private XRRayInteractor _xrRayInteractor;
+
+        private void Awake()
+        {
+            _xrRayInteractor = GetComponent<XRRayInteractor>();
+        }
+
+        private void OnEnable()
+        {
+            _xrRayInteractor.hoverEntered.AddListener(OnHoverEntered);
+            _xrRayInteractor.hoverExited.AddListener(OnHoverExited);
+        }
+
+        private void OnDisable()
+        {
+            _xrRayInteractor.hoverEntered.RemoveListener(OnHoverEntered);
+            _xrRayInteractor.hoverExited.RemoveListener(OnHoverExited);
+        }
+
+        private void OnHoverEntered(HoverEnterEventArgs args)
+        {
+            if (args.interactableObject is TeleportationAnchor)
+            {
+                reticleRender.SetActive(false);
+            }
+        }
+    
+        private void OnHoverExited(HoverExitEventArgs args)
+        {
+            if (args.interactableObject is TeleportationAnchor)
+            {
+                reticleRender.SetActive(true);
+            }
+        }
+    }
+}
