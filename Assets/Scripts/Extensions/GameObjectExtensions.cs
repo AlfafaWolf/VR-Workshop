0a151e3273bb3c20705cf24ab328d439e3dd7163
AlfafaWolf <gabriel.meynard@gmail.com>
2022-06-20 16:35:28 -0300

    feat: add GameObjectExtensions script

---
+++ Assets/Scripts/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace VRWorkshop.Extensions
+{
+    public static class GameObjectExtensions
+    {
+        public static bool TryGetComponentInParent<T>(this GameObject gameObject, out T component) where T : Component
+        {
+            component = gameObject.GetComponentInParent<T>();
+            return component != null;
+        }
+
+        public static bool TryGetComponentInChildren<T>(this GameObject gameObject, out T component) where T : Component
+        {
+            component = gameObject.GetComponentInChildren<T>();
+            return component != null;
+        }
+    }
+}
