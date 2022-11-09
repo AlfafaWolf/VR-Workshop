0ee00011441a75cc6f9925ffde50cd7a2b71eb48
AlfafaWolf <gabriel.meynard@gmail.com>
2022-11-09 18:38:24 -0300

    refactor: update how XR Teleport Controller gets the controller type
    - Added helper class XRHelpers.
    - Changed the way the Teleport Controller identifies the vr controller type.

--- Assets/Scripts/XR/Teleport/XRTeleportController.cs
+++ Assets/Scripts/XR/Teleport/XRTeleportController.cs
@@ -1,31 +1,42 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
-using UnityEngine.XR.OpenXR.Features.Interactions;
-using VRWorkshop.Extensions;
+using InputDevice = UnityEngine.XR.InputDevice;
 
 namespace VRWorkshop.XR.Teleport
 {
     public class XRTeleportController : MonoBehaviour
     {
+        [Header("Settings")]
+        [SerializeField] private InputDeviceCharacteristics controllerCharacteristics = InputDeviceCharacteristics.Controller;
+        
         [Header("Input")]
-        [SerializeField] private InputActionProperty selectAction;
-        [SerializeField] private InputActionProperty selectValueAction;
-        [SerializeField] private InputActionProperty activateAction;
+        [SerializeField] private InputActionProperty primary2DAxisPressAction;
+        [SerializeField] private InputActionProperty primary2DAxisValueAction;
 
         [Header("Dependencies")]
         [SerializeField] private XRRayInteractor rayInteractor;
         [SerializeField] private XRInteractorLineVisual lineVisualInteractor;
         
-        private InteractionLayerMask _interactionLayers;
+        [Header("Events")]
+        public UnityEvent onActivateTeleport;
+        public UnityEvent onCancelTeleport;
+        
+        private InteractionLayerMask _teleportInteractionLayers;
         private bool _isTeleportActive;
         private bool _isPrimary2DAxisPressed;
         private Vector2 _primary2DAxis;
-        private bool _isViveController;
-        private bool _wasTeleportActivated;
+        private bool _wasTeleportSelect;
+        private InputDevice _device;
+        private XRControllerModelType _controllerModel;
 
         private const float TeleportSelectThreshold = 0.7f;
+        private const float TeleportActivateThreshold = 0.6f;
 
         private void Awake()
         {
@@ -35,63 +46,155 @@ namespace VRWorkshop.XR.Teleport
             if (lineVisualInteractor == null)
                 lineVisualInteractor = GetComponent<XRInteractorLineVisual>();
 
-            _interactionLayers = rayInteractor.interactionLayers;
+            _teleportInteractionLayers = rayInteractor.interactionLayers;
         }
 
         private void Start()
         {
-            DisableTeleport();
+            InitDevice();
+            DisableTeleportComponents();
         }
 
         private void OnEnable()
         {
-            selectAction.EnableDirectAction();
-            activateAction.EnableDirectAction();
-            selectValueAction.EnableDirectAction();
-            
-            activateAction.action.performed += OnTeleportActivate;
-            activateAction.action.canceled += OnTeleportCancel;
+            primary2DAxisPressAction.EnableDirectAction();
+            primary2DAxisValueAction.EnableDirectAction();
+            InputDevices.deviceConnected += RegisterDevice;
         }
-
+        
         private void OnDisable()
         {
-            activateAction.action.performed -= OnTeleportActivate;
-            activateAction.action.canceled -= OnTeleportCancel;
-
-            selectAction.DisableDirectAction();
-            activateAction.DisableDirectAction();
-            selectValueAction.DisableDirectAction();
+            primary2DAxisPressAction.DisableDirectAction();
+            primary2DAxisValueAction.DisableDirectAction();
+            InputDevices.deviceConnected -= RegisterDevice;
         }
-
+        
         private void Update()
         {
+            CheckTeleportActivateInput();
             CheckTeleportSelectInput();
+        }
+
+        private void InitDevice()
+        {
+            List<InputDevice> devices = new List<InputDevice>();
+            InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+            _device = devices.FirstOrDefault();
+            _controllerModel = XRHelpers.GetInputDeviceControllerModel(_device);
+        }
+        
+        private void RegisterDevice(InputDevice connectedDevice)
+        {
+            if (!connectedDevice.isValid) return;
+            
+            if ((connectedDevice.characteristics & controllerCharacteristics) == controllerCharacteristics)
+            {
+                _device = connectedDevice;
+                _controllerModel = XRHelpers.GetInputDeviceControllerModel(_device);
+            }
         }
 
         private void CheckTeleportSelectInput()
         {
-            _isPrimary2DAxisPressed = selectAction.action?.IsPressed() ?? false;
-            _primary2DAxis = selectValueAction.action?.ReadValue<Vector2>() ?? Vector2.zero;
-            _isViveController = selectValueAction.action?.activeControl?.device is HTCViveControllerProfile.ViveController;
-
-            if (_primary2DAxis.y >= TeleportSelectThreshold && !_isViveController)
+            if (_controllerModel == XRControllerModelType.HTCVive)
             {
-                _wasTeleportActivated = true;
+                CheckTeleportSelectHtcViveInput();
             }
-            else if (_isPrimary2DAxisPressed && _isViveController)
+            else
             {
-                if (_primary2DAxis.y >= TeleportSelectThreshold)
+                CheckTeleportSelectOculusTouchInput();
+            }
+        }
+        
+        private void CheckTeleportActivateInput()
+        {
+            if (_controllerModel == XRControllerModelType.HTCVive)
+            {
+                CheckTeleportActivateHtcViveInput();
+            }
+            else
+            {
+                CheckTeleportActivateOculusTouchInput();
+            }
+        }
+
+        private void CheckTeleportActivateHtcViveInput()
+        {
+            _isPrimary2DAxisPressed = primary2DAxisPressAction.action?.IsPressed() ?? false;
+            _primary2DAxis = primary2DAxisValueAction.action?.ReadValue<Vector2>() ?? Vector2.zero;
+            
+            if (_isPrimary2DAxisPressed)
+            {
+                if (_primary2DAxis.y >= TeleportActivateThreshold)
                 {
-                    _wasTeleportActivated = true;
+                    if (_isTeleportActive)
+                        return;
+                    
+                    EnableTeleport();
                 }
             }
             else
             {
-                if (_wasTeleportActivated)
+                if (!_isTeleportActive)
+                    return;
+
+                if (_wasTeleportSelect)
                 {
-                    _wasTeleportActivated = false;
                     PerformTeleport();
+                    _wasTeleportSelect = false;
                 }
+                
+                DisableTeleport();
+            }
+        }
+
+        private void CheckTeleportSelectHtcViveInput()
+        {
+            _isPrimary2DAxisPressed = primary2DAxisPressAction.action?.IsPressed() ?? false;
+            _primary2DAxis = primary2DAxisValueAction.action?.ReadValue<Vector2>() ?? Vector2.zero;
+            
+            if (_isPrimary2DAxisPressed)
+            {
+                if (_primary2DAxis.y >= TeleportSelectThreshold)
+                {
+                    _wasTeleportSelect = true;
+                }
+            }
+        }
+        
+        private void CheckTeleportActivateOculusTouchInput()
+        {
+            _primary2DAxis = primary2DAxisValueAction.action?.ReadValue<Vector2>() ?? Vector2.zero;
+            
+            if (_primary2DAxis.y >= TeleportActivateThreshold)
+            {
+                if (_isTeleportActive)
+                    return;
+                
+                EnableTeleport();
+            }
+            else
+            {
+                if (!_isTeleportActive)
+                    return;
+
+                if (_wasTeleportSelect)
+                {
+                    PerformTeleport();
+                    _wasTeleportSelect = false;
+                }
+                
+                DisableTeleport();
+            }
+        }
+        
+        private void CheckTeleportSelectOculusTouchInput()
+        {
+            _primary2DAxis = primary2DAxisValueAction.action?.ReadValue<Vector2>() ?? Vector2.zero;
+            
+            if (_primary2DAxis.y >= TeleportSelectThreshold)
+            {
+                _wasTeleportSelect = true;
             }
         }
 
@@ -99,42 +202,40 @@ namespace VRWorkshop.XR.Teleport
         {
             if (!_isTeleportActive)
                 return;
-
+            
             if (!rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit))
                 return;
 
-            if (!hit.transform.gameObject.TryGetComponentInParent(out BaseTeleportationInteractable interactable))
+            var interactable = hit.transform.GetComponentInParent<BaseTeleportationInteractable>();
+            if (interactable == null)
                 return;
 
             bool hasInteractionLayerOverlap = (rayInteractor.interactionLayers & interactable.interactionLayers) != 0;
             
             if (!hasInteractionLayerOverlap)
                 return;
-            
-            rayInteractor.interactionManager.SelectEnter(rayInteractor as IXRSelectInteractor, interactable as IXRSelectInteractable);
-        }
 
-        private void OnTeleportActivate(InputAction.CallbackContext callbackContext)
-        {
-            EnableTeleport();
-        }
-        
-        private void OnTeleportCancel(InputAction.CallbackContext callbackContext)
-        {
-            DisableTeleport();
+            rayInteractor.interactionManager.SelectEnter(rayInteractor as IXRSelectInteractor, interactable as IXRSelectInteractable);
         }
 
         private void EnableTeleport()
         {
-            rayInteractor.interactionLayers = _interactionLayers;
+            rayInteractor.interactionLayers = _teleportInteractionLayers;
             lineVisualInteractor.enabled = true;
             lineVisualInteractor.reticle.SetActive(true);
             _isTeleportActive = true;
+            onActivateTeleport?.Invoke();
         }
         
         private void DisableTeleport()
         {
             _isTeleportActive = false;
+            DisableTeleportComponents();
+            onCancelTeleport?.Invoke();
+        }
+
+        private void DisableTeleportComponents()
+        {
             rayInteractor.interactionLayers = 0;
             lineVisualInteractor.enabled = false;
             lineVisualInteractor.reticle.SetActive(false);
---
+++ Assets/Scripts/XR/XRHelpers.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine.XR;
+
+namespace VRWorkshop.XR
+{
+    public static class XRHelpers
+    {
+        public static readonly string OculusTouchControllerName = "Oculus Touch Controller OpenXR";
+        public static readonly string HTCViveControllerName = "HTC Vive Controller OpenXR";
+        public static readonly string ValveIndexControllerName = "Index Controller OpenXR";
+        
+        public static XRControllerModelType GetInputDeviceControllerModel(string deviceName)
+        {
+            if (!string.IsNullOrEmpty(deviceName))
+            {
+                if (deviceName.Contains(HTCViveControllerName))
+                {
+                    return XRControllerModelType.HTCVive;
+                }
+                if (deviceName.Contains(OculusTouchControllerName))
+                {
+                    return XRControllerModelType.OculusTouch;
+                }
+                if (deviceName.Contains(ValveIndexControllerName))
+                {
+                    return XRControllerModelType.ValveIndex;
+                }
+            }
+            
+            return XRControllerModelType.None;
+        }
+        
+        public static XRControllerModelType GetInputDeviceControllerModel(InputDevice device)
+        {
+            return GetInputDeviceControllerModel(device.name);
+        }
+    }
+    
+    public enum XRControllerModelType
+    {
+        None,
+        
+        /// <summary>
+        /// Oculus Touch Controller.
+        /// </summary>
+        OculusTouch,
+        
+        /// <summary>
+        /// HTC Vive Controller.
+        /// </summary>
+        HTCVive,
+        
+        /// <summary>
+        /// Valve Index Controller
+        /// </summary>
+        ValveIndex
+    }
+}
+
